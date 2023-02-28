7ad5b268d7fc438c29599bc86d508ea5b2d4bfba
rajuponnamanda <rajuponnamanda3239@gmail.com>
2023-02-28 16:20:58 +0530

    added auth 2

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export const msalConfig ={
     auth :{
         clientId:'2b71bf12-c3de-4bec-8a21-d768b9add662',
         authority:'https://login.microsoftonline.com/{8f6bd982-92c3-4de0-985d-0e287c55e379}',
-        redirectUri:  ' http://localhost:2323',
+        redirectUri: window.location.origin,
     },
 cache : {
     cacheLocation:'localStorage',
